d23add2961823fa0323f5d0d65170819131b2bfa
NaderFarhat <35245275+NaderFarhat@users.noreply.github.com>
2019-12-08 01:29:46 -0300

    make it better

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,14 +1,11 @@
 class Repository < ApplicationRecord
     validates :name, :presence => true
-<<<<<<< HEAD
     validates :login_name, :presence => true
     validates :stars, :presence => true
     validates :language, :presence => true
-=======
     validates :name, :uniqueness => true
     validates :login_name, :presence => true
     validates :stars, :presence => true
     validates :language, :presence => true
     
->>>>>>> make it better
 end
